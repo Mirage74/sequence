b502517e4d5965c7cad2b12953d0fe2c93377b0f
Balex <balexvicx@gmail.com>
2021-03-17 23:13:52 +0100

    teterttet

--- src/components/seq/satellit/addseq.js
+++ src/components/seq/satellit/addseq.js
@@ -14,85 +14,95 @@ export const AddSeq = (params) => {
     const { handleChange, handleSubmit, state } = useSeq();
     const { refreshCB } = params
 
-    console.log (typeof (params.ext))
+    console.log(typeof (params.ext))
     let forRender
     if (!params.ext) {
         forRender = (<>
             <Row>
-            
-            <Col sm={{ span: 1, offset: 5 }} className="" >
-            <h1><i onClick={e => params.PlusMinusCB()}  className="fas fa-plus" style={{ color: '#42e6f5' }}></i></h1>
-                    </Col>
-            
+
+                <Col sm={{ span: 1, offset: 1 }} className="" >
+                    <h1><i onClick={e => params.PlusMinusCB()} className="fas fa-plus-square" style={{ color: '#42e6f5' }}></i></h1>
+                </Col>
+
             </Row>
-            </>)
-            } else {
-                forRender = (<>
-                    <Row>
-                        <Col sm={{ span: 1, offset: 1 }}>
-                            <b>First a<sub></sub>:</b>
-                        </Col>
-                        <Col sm={{ span: 2, offset: 0 }}>
-                            <div style={{ marginBottom: 5 }} className="input-group">
-                                <input type="text" onChange={handleChange} name="a1_index" placeholder="Enter index" />
-                            </div>
-                        </Col>
+        </>)
+    } else {
+        forRender = (<>
+            <Row>
+
+                <Col style={{border: "#123123"}} sm={{ span: 1, offset: 1 }} className="" >
+                    <h1><i onClick={e => params.PlusMinusCB()} className="fas fa-minus-square" style={{ color: '#42e6f5' }}></i></h1>
+                </Col>
+
+            </Row>
+            <Row>
+                <Col sm={{ span: 1, offset: 1 }}>
+                    <b>First a<sub></sub>:</b>
+                </Col>
+                <Col sm={{ span: 2, offset: 0 }}>
+                    <div style={{ marginBottom: 5 }} className="input-group">
+                        <input type="text" onChange={handleChange} name="a1_index" placeholder="Enter index" />
+                    </div>
+                </Col>
 
 
 
 
-                        <Col sm={{ span: 1, offset: 0 }}>
-                            <b>Second a<sub></sub>:</b>
-                        </Col>
-                        <Col sm={{ span: 2, offset: 0 }}>
-                            <div style={{ marginBottom: 5 }} className="input-group">
-                                <input type="text" onChange={handleChange} name="a2_index" placeholder="Enter index" />
-                            </div>
-                        </Col>
-                    </Row>
+                <Col sm={{ span: 1, offset: 0 }}>
+                    <b>Second a<sub></sub>:</b>
+                </Col>
+                <Col sm={{ span: 2, offset: 0 }}>
+                    <div style={{ marginBottom: 5 }} className="input-group">
+                        <input type="text" onChange={handleChange} name="a2_index" placeholder="Enter index" />
+                    </div>
+                </Col>
+            </Row>
 
 
-                    <Row>
-                        <Col sm={{ span: 1, offset: 1 }}>
-                            <b>First a<sub></sub>:</b>
-                        </Col>
-                        <Col sm={{ span: 2, offset: 0 }}>
-                            <div style={{ marginBottom: 5 }} className="input-group">
-                                <input type="text" onChange={handleChange} name="a1_value" placeholder="Enter value" />
-                            </div>
-                        </Col>
-
-
-
-                        <Col sm={{ span: 1, offset: 0 }}>
-                            <b>Second a:</b>
-                        </Col>
-                        <Col sm={{ span: 2, offset: 0 }}>
-                            <div style={{ marginBottom: 5 }} className="input-group">
-                                <input type="text" onChange={handleChange} name="a2_value" placeholder="Enter value" />
-                            </div>
-                        </Col>
-                    </Row>
-
-
-                    <Row>
-                        <Col sm={{ span: 1, offset: 1 }}>
-                            <b>n:</b>
-                        </Col>
-                        <Col sm={{ span: 2, offset: 0 }}>
-                            <div style={{ marginBottom: 5 }} className="input-group">
-                                <input type="text" onChange={handleChange} name="n_index" placeholder="Enter value" />
-                            </div>
-                        </Col>
+            <Row>
+                <Col sm={{ span: 1, offset: 1 }}>
+                    <b>First a<sub></sub>:</b>
+                </Col>
+                <Col sm={{ span: 2, offset: 0 }}>
+                    <div style={{ marginBottom: 5 }} className="input-group">
+                        <input type="text" onChange={handleChange} name="a1_value" placeholder="Enter value" />
+                    </div>
+                </Col>
 
 
 
-                        <Col sm={{ span: 1, offset: 0 }}>
-                            <Button type="submit" className="bg-primary" onClick={e => handleSubmit(state.newSeq, refreshCB)}>Create</Button>
-                        </Col>
-                    </Row>
-                </>)
-            }
+                <Col sm={{ span: 1, offset: 0 }}>
+                    <b>Second a:</b>
+                </Col>
+                <Col sm={{ span: 2, offset: 0 }}>
+                    <div style={{ marginBottom: 5 }} className="input-group">
+                        <input type="text" onChange={handleChange} name="a2_value" placeholder="Enter value" />
+                    </div>
+                </Col>
+            </Row>
+
+
+            <Row>
+                <Col sm={{ span: 1, offset: 1 }}>
+                    <b>n:</b>
+                </Col>
+                <Col sm={{ span: 2, offset: 0 }}>
+                    <div style={{ marginBottom: 5 }} className="input-group">
+                        <input type="text" onChange={handleChange} name="n_index" placeholder="Enter value" />
+                    </div>
+                </Col>
+
+
+
+                <Col sm={{ span: 1, offset: 0 }}>
+                    <Button type="submit" className="bg-primary" onClick={e => handleSubmit(state.newSeq, refreshCB)}>Create</Button>
+                </Col>
+            </Row>
+
+
+
+        </>)
+    }
 
     //console.log("state ", state.newSeq)
 
@@ -105,9 +115,9 @@ export const AddSeq = (params) => {
 
     return (<>
 
-                {forRender}
+        {forRender}
 
-            </>
+    </>
     )
 
 }
--- src/components/seq/satellit/elem2.js
+++ src/components/seq/satellit/elem2.js
@@ -14,26 +14,26 @@ export const Element = (params) => {
 
     let forRender = oneObj && (
         <>
-                <Row style={{ width: "100%", marginLeft: "0px" }}>
+                <Row className="block-example border border-dark" style={{ width: "100%", marginLeft: "0px", fontSize: "1.3rem", fontWeight: 'bold'}}>
                     <Col sm={{ span: 1, offset: 0 }}>
                         <h4><Badge variant="info">{oneObj.num}</Badge></h4>
                     </Col>
-                    <Col sm={{ span: 1, offset: 0 }} className="bg-light">
+                    <Col sm={{ span: 1, offset: 0 }} className="">
                         a<sub>{oneObj.a1_index}</sub> = {oneObj.a1_value}
                     </Col>
-                    <Col sm={{ span: 1, offset: 0 }} className="bg-light">
+                    <Col sm={{ span: 1, offset: 0 }} className="">
                         a<sub>{oneObj.a2_index}</sub> = {oneObj.a2_value}
                     </Col>
-                    <Col sm={{ span: 1, offset: 0 }} className="bg-light" >
+                    <Col sm={{ span: 1, offset: 0 }} className="" >
                         n = {oneObj.n_index}
                     </Col>
-                    <Col sm={{ span: 1, offset: 1 }} className="bg-light" >
+                    <Col sm={{ span: 1, offset: 1 }} className="" >
                         Result:
                     </Col>
-                    <Col sm={{ span: 1, offset: 0 }} className="bg-light" >
+                    <Col sm={{ span: 1, offset: 0 }} className="" >
                         {state.res}
                     </Col>
-                    <Col sm={{ span: 1, offset: 0 }} className="bg-light" >
+                    <Col sm={{ span: 1, offset: 0 }} className="" >
                         <Button className="bg-primary" onClick={(e) => solveHandle(oneObj)}>SOLVE</Button>
                     </Col>
                     <Col sm={{ span: 1, offset: 2 }} className="text-right" >
@@ -56,7 +56,7 @@ export const Element = (params) => {
                     </Col>
 
                 </Row>
-
+                            <br></br>
         </>
     )
 
